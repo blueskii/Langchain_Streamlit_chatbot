a13c1037932c8100ca2b60092a00f11651224492
Shin YongKwon <blueskii@naver.com>
2025-01-28 16:23:46 +0900

    .
    .

--- streamlit_rag_upload_deployment.py
+++ streamlit_rag_upload_deployment.py
@@ -10,7 +10,7 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
 # pysqlite3라는 라이브러리를 동적으로 임포트한 후, 이를 표준 라이브러리의 sqlite3 모듈로 대체
-__import__('pysqlite3')
+import pysqlite3
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain_chroma import Chroma
@@ -19,8 +19,8 @@ from langchain_chroma import Chroma
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 #Chroma tenant 오류 방지 위한 코드
-#import chromadb
-#chromadb.api.client.SharedSystemClient.clear_system_cache()
+import chromadb
+chromadb.api.client.SharedSystemClient.clear_system_cache()
 
 #cache_resource로 한번 실행한 결과 캐싱해두기
 @st.cache_resource
